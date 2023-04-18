e74a7fbd1a2bd81edda8a8ba4f518c45abf45b05
Daniyal <daniyalishfaq10@gmail.com>
2023-04-19 04:52:23 +0500

    loading an image

--- image_noise_reduction.cpp
+++ image_noise_reduction.cpp
@@ -6,6 +6,15 @@ using namespace cv;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string image_path = "../images/example.jpeg";
+    Mat original_image = imread(image_path, IMREAD_COLOR);
+    resize(original_image, original_image, {500, 500});
+    if (original_image.empty())
+    {
+        cout << "Could not load image" << endl;
+        return -1;
+    }
+    imshow("Original Image", original_image);
+    waitKey(0);
     return 0;
 }
